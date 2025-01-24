c9c1c935a6333a19bc1d62977062d0682840a9cf
Ishan Sangani <ishansangani25@gmail.com>
2025-01-24 19:54:00 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,12 @@ app.use(cookieParser());
 
 // CORS configuration
 const corsOptions = {
-    origin: ["https://innov-stock-9f7f.vercel.app", "http://localhost:3000","https://innov-stock.vercel.app"],
+    origin: [
+        "https://innov-stock-9f7f.vercel.app", 
+        "http://localhost:3000",
+        "https://innov-stock.vercel.app",
+        "https://back-finz.vercel.app"
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'x-csrf-token'],
